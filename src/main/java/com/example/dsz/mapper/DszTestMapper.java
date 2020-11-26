55925515e673d3ac3050821aafcdeb9561bc500d
dszazhy1314@163.com <shouzhi.duan@nvxclouds.com>
2020-11-26 17:51:26 +0800

    fix code

---
+++ src/main/java/com/example/dsz/mapper/DszTestMapper.java
@@ -0,0 +1,7 @@
+package com.example.dsz.mapper;
+
+import com.example.dsz.base.MyTkMapper;
+import com.example.dsz.model.DszTest;
+
+public interface DszTestMapper extends MyTkMapper<DszTest> {
+}
---
+++ src/main/java/com/example/dsz/mapper/MyIsamTestMapper.java
@@ -0,0 +1,7 @@
+package com.example.dsz.mapper;
+
+import com.example.dsz.base.MyTkMapper;
+import com.example.dsz.model.MyIsamTest;
+
+public interface MyIsamTestMapper extends MyTkMapper<MyIsamTest> {
+}
