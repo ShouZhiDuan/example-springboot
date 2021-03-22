d8aeee3eceffc16ae8442e326010c291587f66ba
dszazhy1314@163.com <shouzhi.duan@nvxclouds.com>
2021-03-22 09:23:50 +0800

    fix code

---
+++ src/main/java/com/example/dsz/mapper/SchoolMapper.java
@@ -0,0 +1,7 @@
+package com.example.dsz.mapper;
+
+import com.example.dsz.base.MyTkMapper;
+import com.example.dsz.model.School;
+
+public interface SchoolMapper extends MyTkMapper<School> {
+}
---
+++ src/main/java/com/example/dsz/model/School.java
@@ -0,0 +1,13 @@
+package com.example.dsz.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+@Data
+@Table(name = "school")
+public class School {
+    @Id
+    private Integer id;
+    @Column(name = "school_name")
+    private String schoolName;
+}
