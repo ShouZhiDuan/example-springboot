0af8f653278c94379b69b53800de3b9db25b9d79
dszazhy1314@163.com <shouzhi.duan@nvxclouds.com>
2021-04-02 14:59:44 +0800

    fix code

---
+++ src/main/java/com/example/dsz/test/TestDemo.java
@@ -0,0 +1,21 @@
+package com.example.dsz.test;
+
+/**
+ * @Auther: ShouZhi@Duan
+ * @Date: 2021/4/1 13:51
+ * @Description:
+ */
+public class TestDemo {
+    private String a;
+    private final static String name = null;
+    private static int age = 16;
+
+//    public TestDemo(String name) {
+//        this.name = name;
+//    }
+
+//    public static void main(String[] args) {
+//        TestDemo testDemo = new TestDemo(null);
+//        System.out.println();
+//    }
+}
