6ef8fee7365ac47de60cd31089db8824f80be3a9
dszazhy1314@163.com <shouzhi.duan@nvxclouds.com>
2021-05-08 16:36:23 +0800

    https

---
+++ src/main/java/com/example/dsz/mapper/TxTestMapper.java
@@ -0,0 +1,7 @@
+package com.example.dsz.mapper;
+
+import com.example.dsz.base.MyTkMapper;
+import com.example.dsz.model.TxTest;
+
+public interface TxTestMapper extends MyTkMapper<TxTest> {
+}
---
+++ src/main/java/com/example/dsz/model/TxTest.java
@@ -0,0 +1,55 @@
+package com.example.dsz.model;
+
+import javax.persistence.*;
+
+@Table(name = "tx_test")
+public class TxTest {
+    @Id
+    private Integer id;
+
+    private Integer age;
+
+    private String name;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return age
+     */
+    public Integer getAge() {
+        return age;
+    }
+
+    /**
+     * @param age
+     */
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+}
